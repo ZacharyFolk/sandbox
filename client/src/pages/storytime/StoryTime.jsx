ef9ecf0125ad1e10889cb94bdf451b140782fafa
ZacharyFolk <zacharyfolk@gmail.com>
2023-02-04 21:10:14 -0800

    Closer but.. wtf is this hard

--- client/src/pages/storytime/StoryTime.jsx
+++ client/src/pages/storytime/StoryTime.jsx
@@ -1,88 +1,80 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState } from 'react';
+import './story.css';
 const stories = [
   {
     title: 'Story 1',
-    inputs: [
-      { type: 'adjective', placeholder: 'Adjective 1' },
-      { type: 'noun', placeholder: 'Noun 1' },
-      { type: 'verb', placeholder: 'Verb 1' },
-      { type: 'adjective', placeholder: 'Adjective 2' },
-      { type: 'noun', placeholder: 'Noun 2' },
-      { type: 'verb', placeholder: 'Verb 2' },
-    ],
-    template: `The {{adjective1}} {{noun1}} was {{verb1}} the {{adjective2}} {{noun2}}. Suddenly, they {{verb2}}.`,
+    inputs: ['adjective', 'noun', 'verb', 'verb', 'adjective', 'noun'],
+    template: `The {{1}} {{2}} was {{3}} the {{4}} {{5}}. Suddenly, they {{6}}.`,
   },
   {
     title: 'Story 2',
-    inputs: [
-      { type: 'adjective', placeholder: 'Adjective 1' },
-      { type: 'noun', placeholder: 'Noun 1' },
-      { type: 'verb', placeholder: 'Verb 1' },
-      { type: 'noun', placeholder: 'Noun 2' },
-      { type: 'verb', placeholder: 'Verb 2' },
-    ],
+    inputs: ['adjective', 'noun', 'verb', 'verb', 'adjective', 'noun'],
     template: `The {{adjective1}} {{noun1}} {{verb1}} the {{noun2}}. Then they {{verb2}}.`,
   },
 ];
-
 function MadLibs() {
   const [selectedStory, setSelectedStory] = useState(stories[0]);
-  const [currentInput, setCurrentInput] = useState(0);
-  const [inputValues, setInputValues] = useState({});
-  const [madLibStory, setMadLibStory] = useState('');
 
-  useEffect(() => {
-    if (currentInput === selectedStory.inputs.length) {
-      let story = selectedStory.template;
-      Object.entries(inputValues).forEach(([key, value]) => {
-        story = story.replace(`{{${key}}}`, value);
-      });
-      setMadLibStory(story);
-    }
-  }, [currentInput, inputValues, selectedStory]);
-
-  const handleInputChange = (event) => {
-    setInputValues({ ...inputValues, [event.target.name]: event.target.value });
-  };
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [inputValue, setInputValue] = useState('');
+  const [userInputs, setUserInput] = useState([]);
+  const [finalStory, setFinalStory] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setCurrentInput(currentInput + 1);
+    setUserInput((userInputs) => [...userInputs, inputValue]);
+    setInputValue('');
+    setCurrentIndex(currentIndex + 1);
+
+    if (currentIndex === selectedStory.inputs.length) {
+      createStory();
+    } else {
+      setCurrentIndex(currentIndex + 1);
+    }
   };
 
-  if (currentInput < selectedStory.inputs.length) {
-    return (
-      <div>
-        <select
-          onChange={(e) =>
-            setSelectedStory(
-              stories.find((story) => story.title === e.target.value)
-            )
-          }
-        >
-          {stories.map((story) => (
-            <option key={story.title}>{story.title}</option>
-          ))}
-        </select>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor={selectedStory.inputs[currentInput].type}>
-              {selectedStory.inputs[currentInput].placeholder}
-            </label>
-            <input
-              type='text'
-              id={selectedStory.inputs[currentInput].type}
-              name={selectedStory.inputs[currentInput].type}
-              onChange={handleInputChange}
-            />
-          </div>
+  const createStory = () => {
+    const inputObject = userInputs.reduce((acc, value, index) => {
+      acc[index + 1] = value;
+      return acc;
+    }, {});
+
+    let parsedStory = selectedStory.template;
+    for (const key in inputObject) {
+      parsedStory = parsedStory.replace(`{{${key}}}`, inputObject[key]);
+    }
+
+    console.log(parsedStory);
+    setFinalStory(parsedStory);
+  };
+  return (
+    <div className='madlib'>
+      <select
+        onChange={(e) =>
+          setSelectedStory(
+            stories.find((story) => story.title === e.target.value)
+          )
+        }
+      >
+        {stories.map((story) => (
+          <option key={story.title}>{story.title}</option>
+        ))}
+      </select>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Enter a {selectedStory.inputs[currentIndex]}</label>
+          <input
+            type='text'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+
           <button type='submit'>Submit</button>
-        </form>
-      </div>
-    );
-  }
-  return <p>{madLibStory}</p>;
+        </div>
+      </form>
+      <>{finalStory}</>
+    </div>
+  );
 }
 
 export default MadLibs;
