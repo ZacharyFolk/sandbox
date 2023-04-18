873463f21b5fffc86cf6999ec536ce7a504a9796
ZacharyFolk <zacharyfolk@gmail.com>
2023-04-18 10:57:46 -0700

    Adding path to fix CORS hopefully

--- api/index.js
+++ api/index.js
@@ -33,7 +33,11 @@ mongoose
 // });
 // const upload = multer({ storage: storage });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://zacs.website',
+  })
+);
 // app.post('/api/upload', upload.single('file'), (req, res) => {
 //   res.status(200).json('File has been uploaded');
 //   console.log('file uploaded');
